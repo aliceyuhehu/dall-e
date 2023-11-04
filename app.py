f86a873c1e64df2e9ae55191d79d9df3690cc5eb
Alice Hu <yh353@duke.edu>
2023-11-03 20:52:04 -0400

    heyo joyce my brotha

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify
+import os
+import openai
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -6,14 +8,37 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
-@app.route('/generate-image', methods=['POST'])
+@app.route('/generate-image', methods=['POST', 'GET'])
 def generate_image():
-    # Extract the prompt from the request
-    prompt = request.json.get('prompt')
 
-    # Your DALL-E generation code will go here
-    # For now, we'll just return the prompt as a JSON
-    return jsonify({'prompt': prompt})
+    prompt = "dog with sunglasses"
+    openai.api_key = os.getenv("DALLE_API_KEY")
+
+    if not openai.api_key:
+        return jsonify({'error': 'API key not found'}), 403
+
+    try:
+        # Call the OpenAI API to generate an image
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,  
+            size="1024x1024"
+        )
+
+        # Extract the image URL(s) from the response
+        # Adjust the following line according to the actual response structure
+        image_urls = [data['url'] for data in response['data']]
+
+        # Send back the image URLs in the response
+        return jsonify({'images': image_urls})
+
+    except openai.error.OpenAIError as e:
+        # Handle OpenAI specific errors
+        return jsonify({'error': str(e)}), 500
+    except Exception as e:
+        # Handle other possible errors
+        #return jsonify({'error': 'An error occurred while generating the image'}), 500
+        return render_template('show_image.html', image_url=image_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
